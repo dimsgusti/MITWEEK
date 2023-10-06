aaa29b274a86bfdfb195cc46b886f3d6ebe94fb0
dimsgusticode <dimsgusticode@gmail.com>
2023-10-06 16:57:35 +0800

    adding anlytics

--- mit/app/layout.js
+++ mit/app/layout.js
@@ -2,23 +2,26 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/navbar'
 import NavPhone from './components/navPhone'
-// import Sponsor from './components/sponsor'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'MIT-WEEK 2023',
-  description: 'Mulawarman Informatics Technology',
+  description: 'Mulawarman Informatics Tech Week 2023',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
-        {/* <Sponsor /> */}
         <Navbar />
         <NavPhone />
         {children}
+        <Analytics />
       </body>
     </html>
   )
